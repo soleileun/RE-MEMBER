c63c7db09e14eabcf2e056ab44233f988d35704d
DESKTOP-KVCQHCD\multicampus <myway1513@naver.com>
2020-07-24 14:58:14 +0900

    yoodonggyun : board sql controller error modify

--- src/main/java/com/ssafy/test/controller/BoardController.java
+++ src/main/java/com/ssafy/test/controller/BoardController.java
@@ -157,7 +157,7 @@ public class BoardController {
 	@ApiOperation(value = "글번호에 해당하는 게시글의 정보를 삭제한다. 그리고 DB삭제 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@DeleteMapping("delete/{bno}")
 	public ResponseEntity<String> deleteBoard(@PathVariable int bno) {
-
+		//
 		bService.deletecomments(bno);
 		bService.delete(bno);
 		return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
