81ab4c16d5552f5a75368483a3ac94cb94edd926
suhyunhwang <sidewinder91@naver.com>
2020-07-27 11:08:59 +0900

    [suhyun] signup form update

--- frontend/pjt1_imsi-master/src/components/signup/signupInterest.vue
+++ frontend/pjt1_imsi-master/src/components/signup/signupInterest.vue
@@ -1,116 +1,80 @@
 <template>
-  <div class="signupform">
-    <form action=""><input type="checkbox"></form>
+  <div class="signupinterest">
+    <h3>관심 분야 및 스택</h3>
+    <div>
+      <span v-for="pint in pints" :key="pint.pk">
+        <input type="checkbox" v-model="picks" :value="pint.title"/>{{pint.title}} |
+      </span>
+      <br />
+      <input type="text" v-model="intVal" @keyup.enter="add"/>
+      <button @click="add">직접 입력</button>
+    </div>
+    {{picks}}
     <button @click="goSignin" class="gosignin" :class="{submitable:submitable}">선택 완료</button>
   </div>
 </template>
 
 <script>
-import PV from "password-validator";
-import * as EmailValidator from "email-validator";
-import axios from 'axios';
-
 export default {
-  name: "signupform",
-  created() {
-    this.pwSchema
-      .is()
-      .min(8)
-      .is()
-      .max(100)
-      .has()
-      .digits()
-      .has()
-      .letters();
-  },
-  data: function() {
+  name: "signupinterest",
+  created() {},
+  data: function () {
     return {
-      id: "",
-      pw: "",
-      pwSchema: new PV(),
-      pw2: "",
-      nickname: "",
-      name: "",
-      address1: "",
-      address2: "",
-      phone: "",
-      email: "",
-      git: "",
+      pk: 0,
+      pints: [
+        { 
+          pk:0,
+          title:"frontend"
+        },
+        { 
+          pk:1,
+          title:"backend"
+        },
+        { 
+          pk:2,
+          title:"DB"
+        },
+        { 
+          pk:3,
+          title:"JAVA"
+        },
+        { 
+          pk:4,
+          title:"vue.js"
+        }
+      ],
+      picks: [],
       submitable: false,
-      error: {
-        email: '',
-        pw: '',
-        pw2: ''
-      }
+      intVal: "",
     };
   },
   watch: {
-    pw: function() {
-      this.checkForm();
+    picks: function (a) {
+      this.submitable = a.length > 0 ? true : false;
     },
-    pw2: function() {
-      this.checkForm();
-    },
-    email: function() {
-      this.checkForm();
-    }
   },
   methods: {
-    checkForm() {
-      if (this.email.length >= 0 && !EmailValidator.validate(this.email))
-        this.error.email = "이메일 형식이 아닙니다.";
-      else this.error.email = '';
-
-      if (this.pw.length >= 0 && !this.pwSchema.validate(this.pw))
-        this.error.pw = "영문,숫자 포함 8 자리이상이어야 합니다.";
-      else this.error.pw = '';
-
-      if (this.pw !== this.pw2)
-        this.error.pw2 = "비밀번호가 일치하지 않습니다.";
-      else this.error.pw2 = '';
-
-      let submitable = true;
-      Object.values(this.error).map(v => {
-        if (v) submitable = false;
-      });
-      this.submitable = submitable;
-    },
-    goSignin: function() {
-      let form = new FormData();
-      form.append("id", this.id);
-      form.append("pw", this.pw);
-      form.append("pw2", this.pw2);
-      form.append("nickname", this.nickname);
-      form.append("name", this.name);
-      form.append("address1", this.address1);
-      form.append("address2", this.address2);
-      form.append("phone", this.phone);
-      form.append("email", this.email);
-      form.append("git", this.git);
-      console.log(form);
-      axios({
-        method: "post",
-        url: "회원가입 url",
-        data: form,
-        responseType: "json"
-      })
-        .then(response => {
-          console.log(response.data);
-        })
-        .catch(e => {
-          console.log(e);
+    goSignin: function () {},
+    add: function () {
+      if (this.intVal !== "") {
+        this.pints.push({
+          pk: ++this.pk,
+          title: this.intVal,
         });
-    }
-  }
+        this.picks.push(this.intVal);
+      }
+      this.intVal = "";
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.gosignin{
-  opacity: .2;
+.gosignin {
+  opacity: 0.2;
 }
-.submitable{
+.submitable {
   opacity: 1;
 }
 </style>
