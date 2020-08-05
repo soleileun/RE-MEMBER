337c0dfdb863f4ce737183562ac3010d9ce5486f
DESKTOP-KVCQHCD\multicampus <myway1513@naver.com>
2020-08-05 10:33:31 +0900

    [rollback]롤백 부트미적용

---
+++ frontend/pjt1_imsi-master/src/components/mainpage/mainpage1 copy.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="main1">
+  인터레스트 입력을 토대로 추천 position 보여주기
+
+ 
+       
+    
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'main1',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+// .maina{
+// }
+
+
+
+</style>
