8fd2037a9f8b1363265c5bf36a183413c4191c4d
EUNJOUNG <raemong21@cau.ac.kr>
2020-08-06 15:44:00 +0900

    [유저] 유저 탈퇴 기능 수정

--- frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/user/leave.vue
+++ frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/user/leave.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="leave">
-    <h4>회원을 탈퇴하면 복구할 수 없습니다.</h4>
-    비밀번호를 다시 입력해 주세요
-    <br><input type="password" v-model="pw" @keyup.enter="leavemodal"> <button @click="leavemodal">탈퇴하기</button>
+    <h4>회원을 탈퇴하면 복구할 수 없습니다.</h4>비밀번호를 다시 입력해 주세요
+    <br />
+    <input type="password" v-model="pw" @keyup.enter="leavemodal" />
+    <button @click="leavemodal">탈퇴하기</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "leave",
-  data: function() {
-    return{
-      pw:''
-    }
+  data: function () {
+    return {
+      pw: "",
+    };
   },
-  methods:{
-    leavemodal:function(){
-      if (this.pw!=='') {
-        this.$store.dispatch("leave", { pw:this.pw });
+  methods: {
+    leavemodal: function () {
+      if (this.pw !== "") {
+        console.log(this.pw);
+        this.$store.dispatch("leave", { pw: this.pw });
+      } else {
+        alert("비밀번호를 정확하게 입력해주세요");
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
