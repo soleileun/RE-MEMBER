cd80f95af8f976dc15174867f76fb3292f4ca733
EUNJOUNG <raemong21@cau.ac.kr>
2020-08-06 10:11:23 +0900

    [프로필] 초안 추가

--- frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/layout/dashboard/DashboardLayout.vue
+++ frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/layout/dashboard/DashboardLayout.vue
@@ -2,22 +2,23 @@
   <div class="wrapper">
     <side-bar>
       <template slot="links">
-        <sidebar-link to="/main" name="홈" icon="ti-home"/>
-        <sidebar-link to="/pool" name="인재풀" icon="ti-face-smile"/>
-        <sidebar-link to="/recruit" name="모집" icon="ti-search"/>
-        <sidebar-link to="/freeboard" name="자유게시판" icon="ti-clipboard"/>
-        <sidebar-link to="/qaboard" name="질문게시판" icon="ti-help-alt"/>
-        <sidebar-link to="/notice" name="공지사항" icon="ti-announcement"/>
-        <sidebar-link to="/service" name="고객센터" icon="ti-headphone-alt"/>
-        <sidebar-link to="/4444" name="<<<<<<<<<<<<<<<<<<<<<<<<" icon="ti-panel"/>
+        <sidebar-link to="/main" name="홈" icon="ti-home" />
+        <sidebar-link to="/pool" name="인재풀" icon="ti-face-smile" />
+        <sidebar-link to="/recruit" name="모집" icon="ti-search" />
+        <sidebar-link to="/freeboard" name="자유게시판" icon="ti-clipboard" />
+        <sidebar-link to="/qaboard" name="질문게시판" icon="ti-help-alt" />
+        <sidebar-link to="/notice" name="공지사항" icon="ti-announcement" />
+        <sidebar-link to="/service" name="고객센터" icon="ti-headphone-alt" />
+        <sidebar-link to="/profile" name="프로필" icon="ti-face-smile" />
+        <sidebar-link to="/4444" name="<<<<<<<<<<<<<<<<<<<<<<<<" icon="ti-panel" />
 
-        <sidebar-link to="/dashboard" name="Dashboard" icon="ti-panel"/>
-        <sidebar-link to="/stats" name="User Profile" icon="ti-user"/>
-        <sidebar-link to="/table-list" name="Table List" icon="ti-view-list-alt"/>
-        <sidebar-link to="/typography" name="Typography" icon="ti-text"/>
-        <sidebar-link to="/icons" name="Icons" icon="ti-pencil-alt2"/>
-        <sidebar-link to="/maps" name="Map" icon="ti-map"/>
-        <sidebar-link to="/notifications" name="Notifications" icon="ti-bell"/>
+        <sidebar-link to="/dashboard" name="Dashboard" icon="ti-panel" />
+        <sidebar-link to="/stats" name="User Profile" icon="ti-user" />
+        <sidebar-link to="/table-list" name="Table List" icon="ti-view-list-alt" />
+        <sidebar-link to="/typography" name="Typography" icon="ti-text" />
+        <sidebar-link to="/icons" name="Icons" icon="ti-pencil-alt2" />
+        <sidebar-link to="/maps" name="Map" icon="ti-map" />
+        <sidebar-link to="/notifications" name="Notifications" icon="ti-bell" />
       </template>
       <mobile-menu>
         <li class="nav-item">
@@ -26,10 +27,7 @@
             <p>Stats</p>
           </a>
         </li>
-        <drop-down class="nav-item"
-                   title="5 Notifications"
-                   title-classes="nav-link"
-                   icon="ti-bell">
+        <drop-down class="nav-item" title="5 Notifications" title-classes="nav-link" icon="ti-bell">
           <a class="dropdown-item">Notification 1</a>
           <a class="dropdown-item">Notification 2</a>
           <a class="dropdown-item">Notification 3</a>
@@ -48,9 +46,7 @@
     <div class="main-panel">
       <top-navbar></top-navbar>
 
-      <dashboard-content @click.native="toggleSidebar">
-
-      </dashboard-content>
+      <dashboard-content @click.native="toggleSidebar"></dashboard-content>
 
       <content-footer></content-footer>
     </div>
@@ -68,14 +64,14 @@ export default {
     TopNavbar,
     ContentFooter,
     DashboardContent,
-    MobileMenu
+    MobileMenu,
   },
   methods: {
     toggleSidebar() {
       if (this.$sidebar.showSidebar) {
         this.$sidebar.displaySidebar(false);
       }
-    }
-  }
+    },
+  },
 };
 </script>
