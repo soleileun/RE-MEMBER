d47124efae4ac6ed8c26240f8cb7e58fef82af2b
LEEWONOH <suck205@naver.com>
2020-08-06 19:59:59 +0900

    [recruit 테이블]  tag 및 주소로 검색 기능

--- src/main/java/com/ssafy/test/model/service/RecruitService.java
+++ src/main/java/com/ssafy/test/model/service/RecruitService.java
@@ -2,7 +2,10 @@ package com.ssafy.test.model.service;
 
 import java.util.List;
 
+import com.ssafy.test.model.dto.Addr;
+import com.ssafy.test.model.dto.AddrAndTag;
 import com.ssafy.test.model.dto.Recruit;
+import com.ssafy.test.model.dto.TagList;
 
 public interface RecruitService {
 
@@ -15,4 +18,10 @@ public interface RecruitService {
 	public int delete(int rnum);
 
 	public int update(Recruit r);
+	
+	public List<Recruit> selectByAddr(Addr a);
+	
+	public List<Recruit> selectSame(TagList tl);
+	
+	public List<Recruit> selectAddrAndTag(AddrAndTag aat);
 }
