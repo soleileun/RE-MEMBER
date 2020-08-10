5b8fabc231866330099dc954bcbb643f59f4b9c4
EUNJOUNG <raemong21@cau.ac.kr>
2020-08-10 11:20:27 +0900

    [프로젝트] 페이지 수정

--- frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/project/otherproject.vue
+++ frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/project/otherproject.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="project1">
-    <h1>프로젝트 관리 페이지</h1>
+  <div>
+    <h3>{{targetId}}님의 참여 프로젝트</h3>
     <hr />
-    <button @click="movePage" class="btn btn-info btn-round">새로운 프로젝트 생성하기</button>
-    <br />
     <br />
     <div class="row">
       <div class="col-4" v-for="project in projects" :key="project.pid">
@@ -20,18 +18,28 @@ import project from "../project/project1";
 import http from "@/http-common.js";
 const storage = window.sessionStorage;
 export default {
-  name: "myproject",
+  name: "otherproject",
   components: {
     project,
   },
+  props: {
+    userid: String,
+  },
+  watch: {
+    userid: function () {
+      this.init();
+    },
+  },
+
   data: function () {
     return {
       index: 0,
       userNick: storage.getItem("userNick"),
-      userId: this.$route.params.userid,
+      userId: storage.getItem("userid"),
+      targetId: this.$route.params.userid,
+      targetNick: "",
     };
   },
-
   computed: {
     projects() {
       console.log("확인2" + this.$store.state.projectstore.projects);
@@ -40,26 +48,36 @@ export default {
   },
   created() {
     console.log(userId);
+    console.log("userid" + userid);
     this.getPmemberList(this.$route.params.userId);
     this.getProjectListByPmember(this.$route.params.userId);
   },
+  mounted() {
+    this.getProjectListByPmember(this.$route.params.userId);
+  },
+
   methods: {
     getPmemberList(userId) {
       console.log("과정1");
       this.$store.dispatch(Constant.GET_PROJECTLIST_BY_PMEMBER, { userId });
     },
-    movePage() {
-      this.$router.push("/project/makeproject");
-      // console.log(userid);
-      // console.log(userId);
+    init: function () {
+      http
+        .get("/api/searchByUserId/userId=" + this.userid)
+        .then((response) => {
+          if (response.data.length > 0) {
+            console.log("뭘 불러와야하지..");
+          } else {
+            this.content = "아직 프로필이 없습니다";
+          }
+        })
+        .catch((exp) =>
+          alert("내 프로필을 로드하는데에 실패하였습니다." + exp)
+        );
     },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-div.div {
-  display: "inline-block";
-}
+<style>
 </style>
