a223b9f45f54521aad5737d97c5d83333214cf37
EUNJOUNG <raemong21@cau.ac.kr>
2020-08-12 11:19:32 +0900

    [팀원보기] 리스트를 테이블로 구성

--- frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/project/partners.vue
+++ frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/project/partners.vue
@@ -1,20 +1,55 @@
 <template>
   <div>
-    <h4>아직 어디 들어갈지 모르는 팀원관리 ^-^!!</h4>
+    <h4>아직 어디 들어갈지 모르는 팀원관리 ^-^!! 일단 테이블로했는데 카드가 더 이쁠거같아...</h4>
 
     <ul>
       <li v-for="pm in pmlist" :key="pm.userId">{{ pm.userId }}</li>
     </ul>
+
+    <div class="container">
+      <div class="row row--top-40">
+        <div class="col-md-9">
+          <h2 class="row__title">프로젝트 팀원관리</h2>
+        </div>
+      </div>
+      <div class="row row--top-20">
+        <div class="col-md-12">
+          <div class="table-container">
+            <table class="table">
+              <thead class="table__thead">
+                <tr>
+                  <th class="table__th text-center">
+                    <strong>ID</strong>
+                  </th>
+                  <th class="table__th text-center">
+                    <strong>역할</strong>
+                  </th>
+                  <!-- <th class="table__th">프로젝트명</th>
+                  <th class="table__th">마감일시</th>
+                  <th class="table__th">구인현황</th>-->
+                  <!-- <th class="table__th">Progress</th> -->
+                  <!-- <th class="table__th"></th> -->
+                </tr>
+              </thead>
+              <tbody class="table__tbody">
+                <partnerlist v-for="pm in pmlist" :key="pm.userId" :pm="pm" :pid="pm.userId" />
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Constant from "@/Constant.js";
-import http from "@/http-common.js";
+// import http from "@/http-common.js";
+import partnerlist from "@/components/project/partnerlist.vue";
 const storage = window.sessionStorage;
 export default {
   name: "partners",
-  component: {},
+  components: { partnerlist },
   data: function () {
     return {};
   },
