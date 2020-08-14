a9af361bf78e22235e34a1c65ad0f7ed43f53f46
EUNJOUNG <raemong21@cau.ac.kr>
2020-08-14 14:20:32 +0900

    [팀원관리] 팀에서 떠나기 기능 오류

--- frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/project/partners.vue
+++ frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/project/partners.vue
@@ -59,17 +59,30 @@
       <div class="row" v-for="pm in pmlist" :key="pm.userId">
         <template v-if="pm.priority === 1">
           <template v-if="pm.userId === userId">
-            <div class="col-6">
-              <button class="btn btn-info" @click="openModal">+ INVITE NEW TEAM MEMBER</button>
-            </div>
             <div class="col-4">
               <button class="btn btn-warning" @click="openModal2">Change Leader</button>
             </div>
           </template>
         </template>
 
+        <template v-if="pm.userId === userId">
+          <div>{{checkmember(1)}}</div>
+          <div class="col-4">
+            <button class="btn btn-info" @click="openModal">+ INVITE NEW TEAM MEMBER</button>
+          </div>
+        </template>
+        <template v-if="pm.userId === userId">
+          <div class="col-4">
+            <button class="btn btn-danger" @click="openModal3">Leave This Team</button>
+          </div>
+        </template>
+
         <!-- The Modal -->
       </div>
+      <template v-if="test1">
+        <button class="btn btn-info">프로젝트 팀 참여하기</button>
+      </template>
+
       <div id="myModal" class="modal">
         프로젝트 팀원 추가하기
         <div class="modal-content">
@@ -124,6 +137,33 @@
           </div>
         </div>
       </div>
+      <div id="myModal3" class="modal">
+        프로젝트를 떠나요
+        <div class="modal-content">
+          <span class="close">&times;</span>
+          <div class="col-12">
+            <h3>팀 나가기</h3>
+            <br />
+
+            <h5>프로젝트를 떠나려면 아래 문장을 똑같이 입력해주세요</h5>
+            <h4>
+              안녕히 계세요 여러분 ~!
+              <br />저는 이 세상의 모든 굴레와 속박을 벗어던지고
+              <br />제 행복을 찾아 떠납니다~~~!
+            </h4>
+            <fg-input type="text" placeholder="example@naver.com" v-model="bye"></fg-input>
+            <small>정말 팀을 떠나실건가요? 하지만 언제든 지원해 다시 팀에 들어올 수 있어요!</small>
+            <div>
+              <br />
+              <br />
+              <br />
+            </div>
+            <button class="btn btn-info btn-round" @click="leaveTeam">팀 나가기</button>
+            <br />
+            <br />
+          </div>
+        </div>
+      </div>
     </card>
   </div>
 </template>
@@ -140,7 +180,9 @@ export default {
     return {
       newid: "",
       newleaderID: "",
+      bye: "",
       state: "",
+      test1: true,
       title: "Team members",
       pid: this.$route.params.pid,
       userId: storage.getItem("userid"),
@@ -195,6 +237,24 @@ export default {
         }
       };
     },
+    openModal3: function () {
+      console.log(this.userId);
+      var modal = document.getElementById("myModal3");
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+      // When the user clicks on the button, open the modal
+      modal.style.display = "block";
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function () {
+        modal.style.display = "none";
+      };
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      };
+    },
     changeLeader: function () {
       if (this.newleaderID != "") {
         this.$store.dispatch("changeLeader", {
@@ -203,9 +263,16 @@ export default {
         });
       }
     },
+    leaveTeam: function () {
+      // console.log(userId);
+      if (this.bye === "z") {
+        this.$store.dispatch("leaveTeam", {
+          userId: window.localstorage.getItem("userid"),
+          pid: this.$route.params.pid,
+        });
+      }
+    },
     addnewpm: function () {
-      // console.log("프로젝트 아이디 확인하고싶어 ㅠ");
-      //console.log(this.$route.params.pid);
       if (this.state != "" && this.newid != "") {
         this.$store.dispatch("addNewMember", {
           userId: this.newid,
@@ -217,6 +284,9 @@ export default {
 
     checkstate: function (a) {
       this.state = a;
+    },
+    checkmember: function (a) {
+      this.test1 = false;
     },
     getStatusClass(status) {
       switch (status) {
@@ -276,8 +346,5 @@ export default {
 }
 .checks {
   display: inline;
-  span {
-    margin: 10px;
-  }
 }
 </style>
