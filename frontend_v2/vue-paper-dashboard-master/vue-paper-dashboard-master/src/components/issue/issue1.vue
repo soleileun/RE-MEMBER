caddf1c4d9172360da7a9c22febc155bde482040
김영훈 <kyhoon001@naver.com>
2020-08-17 14:48:52 +0900

    [이슈관리] 색깔 입혔음.

--- frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/issue/issue1.vue
+++ frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/issue/issue1.vue
@@ -46,15 +46,30 @@
 
     <div class="content" v-if="issues.length != 0">
       <div class="container-fluid">
-        <div class="row">
-          <div class="col-5">
+        <div class="row" >
+
+          <div class="col-lg-4">
+                                <div class="card " style="width:100%; height:100%;"> 
+                                    <div class="card-header">
+                                        <i class="fas fa-chart-pie mr-1"></i>
+                                        이슈 현황
+                                    </div>
+                                    <div class="card-body"><canvas id="myChart" width="100%" height="80%"></canvas></div>
+                                    <div class="card-footer small text-muted">
+                                      <i class="fa fa-history"></i>
+                    Last Update {{issues[0].changeDay.slice(0,10)}} {{issues[0].changeDay.slice(11,19)}}
+                                    </div>
+                                </div>
+                            </div>
+          <!--
+          <div class="col-5 " style="overflow:auto;">
             <div class="card">
               <div class="header">
                 <h4 class="card-title">이슈 처리 현황 그래프</h4>
               </div>
-              <div class="content">
-                <div class="ct-chart">
-                  <canvas id="myChart" style="height: 50%; width: 50%;"></canvas>
+              <div class="content" style="overflow:auto;">
+                <div class="ct-chart" style="overflow:auto;">
+                  <canvas id="myChart" style="height: 60%; width: 60%; display:block; max-width:300px; max-height:300px;"></canvas>
                 </div>
                 <div class="footer">
                   <hr />
@@ -66,38 +81,40 @@
               </div>
             </div>
           </div>
-
-          <div class="col-7">
+          -->
+          <div class="col-8">
             <div class="card">
-              <div class="header">
-                <h4 class="card-title">최근 프로젝트 이슈</h4>
-                <p align="right" class="category">
+              <div class="card-header">
+                <h4 class="card-title col-8" style="display:inline-block;">변경 이력</h4>
+                <p align="right" class="category col-4" style="display:inline-block; padding:4px;">
                   <button v-on:click="createIssue()" class="btn btn-info btn-simple btn-xs">
                     <i class="fa fa-edit"></i>이슈 생성하기
                   </button>
                 </p>
               </div>
-              <div class="content" style="height:37vh; overflow:scroll;">
-                <div class="overflow-auto table-responsive">
-                  <table class="table">
+              <div class="content" style="height:37vh; overflow-y:scroll; overflow-x:none;">
+                <div class="overflow-auto table-responsive" style="">
+                  <table class="table" style="width:100%;">
                     <thead>
-                      <th>마지막 수정 시간</th>
-                      <th>제목</th>
-                      <th class="td-actions text-right">상태</th>
+                      <th class="td-actions" style="width:15%;">상태</th>
+                      <th style="width:65%;">제목</th>
+                      <th style="width:20%;">시간</th>
                     </thead>
                     <tbody>
                       <tr v-for="(issue, index) in issues" :key="index">
-                        <td>{{issue.changeDay.slice(0,10)}} {{issue.changeDay.slice(11,19)}}</td>
-                        <td>{{issue.issuetitle}}</td>
-                        <td class="td-actions text-right">
-                          <div v-if="issue.issuestate == 'created'" style="color:skyblue">생성됨</div>
-                          <div v-if="issue.issuestate == 'ongoing'" style="color:red">진행중</div>
+                        <td class="td-actions" style="text-align:left;">
+                          <div v-if="issue.issuestate == 'created'" style="color:skyblue">생성</div>
+                          <div v-if="issue.issuestate == 'ongoing'" style="color:red">진행</div>
                           <div v-if="issue.issuestate == 'done'" style="color:yellowgreen">완료</div>
                         </td>
+                        <td>{{issue.issuetitle}}</td>
+                        <td>{{issue.changeDay.slice(5,10)}} / {{issue.changeDay.slice(11,16)}}</td>
                       </tr>
                     </tbody>
                   </table>
                 </div>
+              </div>
+
 
                 <div class="footer">
                   <hr />
@@ -109,18 +126,19 @@
               </div>
             </div>
           </div>
-        </div>
         <!-- drag and drop -->
-        <div class="content container-fluid row col-14 card" v-drag-and-drop:options="options">
-          <h4 class="header card-title">이슈 현황</h4>
-          <p align="right" class="category">
+        <div class="content container-fluid row col-14 card" style="margin-top:20px;" v-drag-and-drop:options="options">
+          <div class="card-header" style="max-height:120px;">
+          <h4 class="header card-title col-8" style="display:inline-block;">이슈 현황</h4>
+          <p align="right" class="category col-4" style="display:inline-block;">
             <button
               v-on:click="updateIssueChange()"
               class="btn btn-info btn-simple btn-xs"
             >변경사항 저장하기</button>
           </p>
           <hr />
-          <ul class="drag-list content">
+          </div>
+          <ul class="drag-list content" style="margin-top:10px;">
             <li class="drag-column" v-for="group in groups" :key="group.id">
               <span class="card-title">
                 <h2 v-if="group.name == 'To Do'" style="color:skyblue">생성된 이슈</h2>
@@ -134,7 +152,7 @@
                 :data-id="group.id"
                 @change="onGroupsChange"
               >
-                <ul class="drag-inner-list" :data-id="group.id">
+                <ul class="drag-inner-list " :data-id="group.id">
                   <li
                     class="drag-item"
                     v-for="item in group.items"
@@ -343,7 +361,7 @@ export default {
         this.issues_done.length,
       ];
       var chDonutData1 = {
-        labels: ["생성된 이슈", "진행중인 이슈", "완료된 이슈"],
+        labels: ["생성", "진행", "완료"],
         datasets: [
           {
             backgroundColor: colors.slice(0, 3),
@@ -646,5 +664,35 @@ ul {
 }
 .navtest:hover {
   color: #ffffff;
+}
+.drag-inner-list[data-id = '1'] > li {
+  border-radius : 15px;
+  background-color: rgba( 135, 206, 235, 0.7 );
+  vertical-align: middle;
+  text-align: center;
+  cursor:Pointer;
+}
+.drag-inner-list[data-id = '1'] > li:active {
+  cursor:move;
+}
+.drag-inner-list[data-id = '2'] > li {
+  background-color: rgba( 255, 0, 0, 0.7 );
+  border-radius : 15px;
+  vertical-align: middle;
+  text-align: center;
+  cursor:Pointer;
+}
+.drag-inner-list[data-id = '2'] > li:active {
+  cursor:move;
+}
+.drag-inner-list[data-id = '3'] > li {
+  background-color: rgba( 154, 205, 50, 0.7 );
+  border-radius : 15px;
+  cursor:Pointer;
+  vertical-align: middle;
+  text-align: center;
+}
+.drag-inner-list[data-id = '3'] > li:active {
+  cursor:move;
 }
 </style>
