6f852f582715ed7916f94dd32bc7a208f34af0ae
EUNJOUNG <raemong21@cau.ac.kr>
2020-08-20 15:34:01 +0900

    [댓글] 삭제 기능 수정

--- frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/comment/comment.vue
+++ frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/components/comment/comment.vue
@@ -1,29 +1,43 @@
 <template>
   <div>
-    {{comment.cwriter}} {{comment.ccontent}} {{comment.makeDay}} 
-    <button @click="deleteComment(comment)">삭제</button>
+    {{comment.cwriter}} {{comment.ccontent}} {{comment.makeDay}}
+    <button
+      @click="deleteComment(comment)"
+    >삭제</button>
   </div>
 </template>
 
 <script>
-import Constant from '../../Constant';
+import Constant from "../../Constant";
+const storage = window.sessionStorage;
 export default {
-  props:{
-    comment : {
-      type : Object,
-      required : true
-    }
+  data() {
+    return {
+      userNick: storage.getItem("userNick"),
+    };
+  },
+  props: {
+    comment: {
+      type: Object,
+      required: true,
+    },
   },
 
-  methods:{
-    deleteComment(comment){
-      this.$store.dispatch(Constant.REMOVE_COMMENT,{cno : comment.cno, bno : comment.bno });
-      
-    }
-  }
-}
+  methods: {
+    deleteComment(comment) {
+      console.log("왜???");
+      if (this.userNick === comment.cwriter) {
+        this.$store.dispatch(Constant.REMOVE_COMMENT, {
+          cno: comment.cno,
+          bno: comment.bno,
+        });
+      } else {
+        aler("삭제할 수 없습니다");
+      }
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
