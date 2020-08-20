f4b7dd50e5f34e76adc43c801ec24e88c98243d7
DESKTOP-KVCQHCD\multicampus <myway1513@naver.com>
2020-08-20 20:46:33 +0900

    [콘솔제거]1

--- frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/http-common.js
+++ frontend_v2/vue-paper-dashboard-master/vue-paper-dashboard-master/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://i3a208.p.ssafy.io:8080/",
   headers: {
     "Content-type": "application/json",
   }
